fb24cebe1a69c39649db939f10477bfa7d415dde
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-05-13 10:37:05 -0300

    Fazendo autenticação Basic Auth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,8 +1,28 @@
 import { Request, Response, NextFunction } from "express"
+import { User } from "../models/User";
 
 export const Auth = {
-    private: (req: Request, res: Response, next: NextFunction) => {
+    private: async (req: Request, res: Response, next: NextFunction) => {
         let success = false;
+
+        if (req.headers.authorization) {
+            let hash: string = req.headers.authorization.substring(6);
+            let decoded: string = Buffer.from(hash, 'base64').toString();
+            console.log(decoded);
+            let data: string[] = decoded.split(':');
+
+            if (data.length === 2) {
+                let hasUser = await User.findOne({
+                    where: {
+                        email: data[0],
+                        password: data[1]
+                    }
+                });
+                if(hasUser) {
+                    success = true;
+                }
+            }
+        }
 
         if (success) {
             next();
