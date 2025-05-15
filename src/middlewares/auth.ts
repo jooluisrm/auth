4010773e8c4b6657b6363260331afdb7aa73bece
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-05-15 09:11:13 -0300

    JWT: Validando o Token

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,27 +1,28 @@
 import { Request, Response, NextFunction } from "express"
 import { User } from "../models/User";
+import JWT from 'jsonwebtoken';
+import dotenv from "dotenv";
 
+dotenv.config();
 export const Auth = {
     private: async (req: Request, res: Response, next: NextFunction) => {
         let success = false;
 
         if (req.headers.authorization) {
-            let hash: string = req.headers.authorization.substring(6);
-            let decoded: string = Buffer.from(hash, 'base64').toString();
-            console.log(decoded);
-            let data: string[] = decoded.split(':');
+            const [authType, token] = req.headers.authorization.split(' ');
 
-            if (data.length === 2) {
-                let hasUser = await User.findOne({
-                    where: {
-                        email: data[0],
-                        password: data[1]
-                    }
-                });
-                if(hasUser) {
+            if (authType === 'Bearer') {
+                try {
+                    const decoded = JWT.verify(
+                        token,
+                        process.env.JWT_SECRET_KEY as string
+                    );
                     success = true;
+                } catch (err) {
+                    
                 }
             }
+
         }
 
         if (success) {
